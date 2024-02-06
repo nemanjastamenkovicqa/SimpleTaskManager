99e146e496f75a7d42e1dfb260f37535d526e2fa
Nemanja Stamenkovic <nemanjastamenkovicqa@gmail.com>
2024-02-06 21:54:32 +0100

    Added sortTasksByCompletion method to TaskSorter

--- src/TaskSorter.java
+++ src/TaskSorter.java
@@ -24,4 +24,13 @@ public class TaskSorter {
 
     // You can add additional sorting methods based on different criteria if needed
 
+    // New function to upgrade TaskSorter
+    public static void sortTasksByCompletion(ArrayList<Task> tasks) {
+        if (!tasks.isEmpty()) {
+            Collections.sort(tasks, Comparator.comparing(Task::isCompleted));
+            System.out.println("Tasks sorted by completion status.");
+        } else {
+            System.out.println("No tasks available to sort.");
+        }
+    }
 }
