4d64e0ba4c7e94275aa151e054e6284c93aa0c01
nemanjastamenkovicqa <145938924+nemanjastamenkovicqa@users.noreply.github.com>
2024-05-16 22:52:37 +0200

    Update TaskPriorityManager.java
    Cleaning the code in SimpleTaskManager

--- src/TaskPriorityManager.java
+++ src/TaskPriorityManager.java
@@ -3,12 +3,10 @@ import java.util.Map;
 class TaskPriorityManager {
     private Map<Integer, Boolean> taskPriorities = new HashMap<>();
 
-    // Method to set task priority
     public void setTaskPriority(int taskId, boolean important) {
         taskPriorities.put(taskId, important);
     }
-
-    // Method to check if a task is important
+    
     public boolean isTaskImportant(int taskId) {
         return taskPriorities.getOrDefault(taskId, false);
     }
